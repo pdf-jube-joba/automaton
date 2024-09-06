6a7d7dc91e505af4c13c0ddc687fc89ceade55f2
pdf <61172336+pdf-jube-joba@users.noreply.github.com>
2024-09-06 12:26:53 +0900

    OK

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,18 @@ use rand::prelude::*;
 use wasm_bindgen::JsValue;
 use yew::prelude::*;
 
+// なんか一気にlogが描画されるせいでちゃんと動いてるのかわかりにくい。
 fn main() {
-    // log("start");
-    // for i in 0..100000000 {
-    //     if i % 10000 == 0 {
-    //         log(format!("{i}"))
-    //     }
-    // }
-    // なんか一気にlogが描画されるせいでちゃんと動いてるのかわかりにくい。
-    let game = train();
+    let mut rng = thread_rng();
+    let genes = train(&mut rng);
     let document = gloo::utils::document();
     let target_element = document.get_element_by_id("onthis").unwrap();
-    yew::Renderer::<App>::with_root_and_props(target_element, StartAppGame { game }).render();
+    yew::Renderer::<App>::with_root_and_props(target_element, StartAppGame { genes }).render();
 }
 
 const W: usize = 200;
 const H: usize = 150;
-const P_FOOD: f64 = 0.3;
+const P_FOOD: f64 = 0.1;
 
 const MAXHP: usize = 15;
 const RECOVER: usize = 5;
@@ -31,26 +26,56 @@ const MUT_NUM: usize = 5;
 const MUT_CPY: usize = 2;
 const MUT_MUCH: usize = 100;
 
-const TRAIN_STEPMAX: usize = 100;
-const TRAIN_NUM: usize = 50;
+const TRAIN_STEPMAX: usize = 1000;
+const TRAIN_NUM: usize = 20;
+const TRAIN_B: usize = 20;
+
+// 表示するときのマスの大きさ
+const LEN: usize = 5;
+
+pub fn log<T: AsRef<str>>(str: T) {
+    web_sys::console::log_1(&JsValue::from_str(str.as_ref()))
+}
 
 #[derive(Debug, Clone, PartialEq, Properties)]
-
 struct Field {
     field: [[bool; W]; H],
 }
 
-pub fn log<T: AsRef<str>>(str: T) {
-    web_sys::console::log_1(&JsValue::from_str(str.as_ref()))
+impl Field {
+    fn random(rng: &mut ThreadRng) -> Self {
+        let mut field = [[false; W]; H];
+        for row in field.iter_mut() {
+            for item in row {
+                if rng.gen_bool(P_FOOD) {
+                    *item = true;
+                }
+            }
+        }
+        Self { field }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 struct Chan {
     pos: (usize, usize),
     hp: usize,
-    act: Gene,
+    gene: Gene,
     mem: u8,
     ori: Ori,
+}
+
+impl Chan {
+    fn spawn(gene: Gene, rng: &mut ThreadRng) -> Self {
+        let pos = (rng.gen_range(0..H), rng.gen_range(0..W));
+        Chan {
+            pos,
+            hp: MAXHP,
+            gene,
+            mem: 0,
+            ori: Ori::U,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -65,7 +90,7 @@ const MOVE_NUM: usize = 2_usize.pow((3 * 3) as u32 - 1) * (2_usize.pow(8));
 
 #[derive(Debug, Clone, PartialEq)]
 struct Gene {
-    act: [Action; MOVE_NUM],
+    gene: [Action; MOVE_NUM],
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -91,13 +116,13 @@ impl Gene {
                 }
             }
         }
-        &self.act[index as usize]
+        &self.gene[index as usize]
     }
 
     fn random(rng: &mut ThreadRng) -> Self {
-        let mut act = [Action::Forword; MOVE_NUM];
-        for i in 0..MOVE_NUM {
-            act[i] = match rng.gen_range(0..4) {
+        let mut gene = [Action::Forword; MOVE_NUM];
+        for gene_i in gene.iter_mut() {
+            *gene_i = match rng.gen_range(0..4) {
                 0 => Action::Forword,
                 1 => Action::Back,
                 2 => Action::TurnLeft,
@@ -105,11 +130,10 @@ impl Gene {
                 _ => unreachable!(),
             };
         }
-        Self { act }
+        Self { gene }
     }
 
-    fn mutate(&mut self, num: usize) {
-        let mut rng = thread_rng();
+    fn mutate(&mut self, num: usize, rng: &mut ThreadRng) {
         for _ in 0..num {
             let i = rng.gen_range(0..MOVE_NUM);
             let o = match rng.gen_range(0..4) {
@@ -119,40 +143,23 @@ impl Gene {
                 3 => Action::TurnRight,
                 _ => unreachable!(),
             };
-            self.act[i] = o;
+            self.gene[i] = o;
         }
     }
 
-    fn mix(&self, other: &Self) -> Self {
-        let mut act = [Action::Forword; MOVE_NUM];
-        let mut rng = thread_rng();
+    fn mix(&mut self, other: &Self, rng: &mut ThreadRng) {
         for i in 0..MOVE_NUM {
-            act[i] = if rng.gen_bool(0.5) {
-                self.act[i]
-            } else {
-                other.act[i]
+            if rng.gen_bool(0.5) {
+                self.gene[i] = other.gene[i];
             }
         }
-        Self { act }
     }
 }
 
-fn goto(pos: (usize, usize), diff: (isize, isize)) -> Option<(usize, usize)> {
-    let i = {
-        let i = pos.0 as isize + diff.0;
-        if i < 0 || H as isize <= i {
-            return None;
-        }
-        i as usize
-    };
-    let j = {
-        let j = pos.1 as isize + diff.1;
-        if j < 0 || W as isize <= j {
-            return None;
-        }
-        j as usize
-    };
-    Some((i, j))
+fn goto(pos: (usize, usize), diff: (isize, isize)) -> (usize, usize) {
+    let i = (pos.0 as isize + diff.0).rem_euclid(H as isize);
+    let j = (pos.1 as isize + diff.1).rem_euclid(W as isize);
+    (i as usize, j as usize)
 }
 
 fn rotate_from_chan(diff: (isize, isize), ori: Ori) -> (isize, isize) {
@@ -172,62 +179,29 @@ struct OneGame {
 }
 
 impl OneGame {
-    fn new() -> Self {
-        let mut rng = thread_rng();
-        let mut field = [[false; W]; H];
-        for i in 0..H {
-            for j in 0..W {
-                if rng.gen_bool(P_FOOD) {
-                    field[i][j] = true;
-                }
-            }
-        }
+    fn random(rng: &mut ThreadRng) -> Self {
+        let field = Field::random(rng);
         // log("A");
         let chans = (0..CHAN_NUM)
-            .map(|_| {
-                let pos = (rng.gen_range(0..H), rng.gen_range(0..W));
-                Ok(Chan {
-                    pos,
-                    hp: MAXHP,
-                    act: Gene::random(&mut rng),
-                    mem: 0,
-                    ori: Ori::U,
-                })
-            })
+            .map(|_| Ok(Chan::spawn(Gene::random(rng), rng)))
             .collect::<Vec<_>>();
         // log("B");
         Self {
             remain: CHAN_NUM,
-            field: Field { field },
+            field,
             chans,
         }
     }
-    fn from_gene(genes: Vec<Gene>) -> Self {
+    fn from_gene(genes: Vec<Gene>, rng: &mut ThreadRng) -> Self {
         assert_eq!(genes.len(), CHAN_NUM);
-        let mut rng = thread_rng();
-        let mut field = [[false; W]; H];
-        for i in 0..H {
-            for j in 0..W {
-                if rng.gen_bool(P_FOOD) {
-                    field[i][j] = true;
-                }
-            }
-        }
-        let mut chans = vec![];
-        for gene in genes {
-            let pos = (rng.gen_range(0..H), rng.gen_range(0..W));
-            let chan = Chan {
-                pos,
-                hp: MAXHP,
-                act: gene,
-                mem: 0,
-                ori: Ori::U,
-            };
-            chans.push(Ok(chan));
-        }
+        let field = Field::random(rng);
+        let chans = genes
+            .into_iter()
+            .map(|gene| Ok(Chan::spawn(gene, rng)))
+            .collect();
         Self {
             remain: CHAN_NUM,
-            field: Field { field },
+            field,
             chans,
         }
     }
@@ -236,80 +210,83 @@ impl OneGame {
         self.remain == 0
     }
 
-    fn end(&mut self) -> Self {
-        let mut genes = nice_gene(&self);
-        // top n の mix
-        let mix_genes = {
-            let mut v = vec![];
-            for i in 0..MIX_NUM {
-                for j in 0..MIX_NUM {
-                    if i != j {
-                        v.push(genes[i].mix(&genes[j]));
-                    }
-                }
-            }
-            v
-        };
-        // top n の mutate
-        let mut_genes = {
-            let mut v = vec![];
-            for i in 0..MUT_NUM {
-                for _ in 0..MUT_CPY {
-                    let mut g = genes[i].clone();
-                    g.mutate(MUT_MUCH);
-                    v.push(g);
-                }
-            }
-            v
-        };
+    fn get_genes_ordered(&self) -> Vec<Gene> {
+        let OneGame {
+            remain: _,
+            field: _,
+            chans,
+        } = self;
 
-        for _ in 0..mix_genes.len() {
-            genes.pop();
-        }
-
-        for _ in 0..mut_genes.len() {
-            genes.pop();
-        }
-
-        genes.extend(mix_genes);
-        genes.extend(mut_genes);
-        OneGame::from_gene(genes)
+        let mut gene_order: Vec<_> = chans
+            .iter()
+            .map(|result| match result {
+                Ok(chan) => ((0, chan.hp), chan.gene.clone()),
+                Err((order, gene)) => ((1, *order), gene.clone()),
+            })
+            .collect();
+        gene_order.sort_by_key(|(o, _)| *o);
+        gene_order.into_iter().map(|(_, g)| g).collect()
     }
 }
 
-fn nice_gene(
-    OneGame {
-        remain,
-        field,
-        chans,
-    }: &OneGame,
-) -> Vec<Gene> {
-    let mut gene_order: Vec<_> = chans
-        .iter()
-        .map(|result| match result {
-            Ok(chan) => ((0, chan.hp), chan.act.clone()),
-            Err((order, gene)) => ((1, *order), gene.clone()),
-        })
-        .collect();
-    gene_order.sort_by_key(|(o, g)| *o);
-    gene_order.into_iter().map(|(o, g)| g).collect()
+fn next_gene(genes: Vec<Gene>, rng: &mut ThreadRng) -> Vec<Gene> {
+    let mut new_genes = Vec::with_capacity(genes.len());
+
+    // top n の mix
+    for i in 0..MIX_NUM {
+        for j in 0..MIX_NUM {
+            if i != j {
+                let mut new_gene = genes[i].clone();
+                new_gene.mix(&genes[j], rng);
+                new_genes.push(new_gene);
+            }
+        }
+    }
+    // top n の mutate
+    for i in 0..MUT_NUM {
+        for _ in 0..MUT_CPY {
+            let mut new_gene = genes[i].clone();
+            new_gene.mutate(MUT_MUCH, rng);
+            new_genes.push(new_gene);
+        }
+    }
+
+    let l = genes.len() - new_genes.len();
+    new_genes.extend(genes[0..l].to_owned());
+    new_genes
 }
 
-fn train() -> OneGame {
+fn train(rng: &mut ThreadRng) -> Vec<Gene> {
     // log("start");
-    let mut game = OneGame::new();
+    let mut game = OneGame::random(rng);
     'a: for i in 0..TRAIN_NUM {
         // log(format!("train {i}"));
         for _ in 0..TRAIN_STEPMAX {
             game.step();
             if game.is_end() {
-                game = game.end();
+                game = OneGame::from_gene(next_gene(game.get_genes_ordered(), rng), rng);
                 continue 'a;
             }
         }
         break 'a;
     }
-    game.end()
+    game.get_genes_ordered()
+}
+
+fn traint_from_gene(genes: Vec<Gene>, num: usize, rng: &mut ThreadRng) -> Vec<Gene> {
+    let mut game = OneGame::from_gene(genes, rng);
+    'a: for i in 0..num {
+        // log(format!("train {i}"));
+        for _ in 0..TRAIN_STEPMAX {
+            game.step();
+            if game.is_end() {
+                game = OneGame::from_gene(next_gene(game.get_genes_ordered(), rng), rng);
+                continue 'a;
+            }
+        }
+        break 'a;
+    }
+    game.get_genes_ordered()
 }
 
 impl OneGame {
@@ -325,24 +302,18 @@ impl OneGame {
             };
             let surr = |diff: (isize, isize)| -> bool {
                 let diff = rotate_from_chan(diff, chan.ori);
-                let Some((i, j)) = goto(chan.pos, diff) else {
-                    return false;
-                };
+                let (i, j) = goto(chan.pos, diff);
                 field.field[i][j]
             };
-            let act = chan.act.get_action(surr, chan.mem);
+            let act = chan.gene.get_action(surr, chan.mem);
             match act {
                 Action::Forword => {
                     let diff = rotate_from_chan((1, 0), chan.ori);
-                    if let Some(pos) = goto(chan.pos, diff) {
-                        chan.pos = pos;
-                    }
+                    chan.pos = goto(chan.pos, diff);
                 }
                 Action::Back => {
-                    let diff = rotate_from_chan((1, 0), chan.ori);
-                    if let Some(pos) = goto(chan.pos, diff) {
-                        chan.pos = pos;
-                    }
+                    let diff = rotate_from_chan((-1, 0), chan.ori);
+                    chan.pos = goto(chan.pos, diff);
                 }
                 Action::TurnRight => {
                     chan.ori = match chan.ori {
@@ -366,7 +337,7 @@ impl OneGame {
                 field.field[chan.pos.0][chan.pos.1] = false;
             }
             if chan.hp == 0 {
-                *chan_opt = Err((*remain, chan.act.clone()));
+                *chan_opt = Err((*remain, chan.gene.clone()));
                 *remain -= 1;
             } else {
                 chan.hp -= 1;
@@ -374,8 +345,6 @@ impl OneGame {
         }
     }
 }
-
-const LEN: usize = 5;
 
 fn onegame_view(
     OneGame {
@@ -386,17 +355,17 @@ fn onegame_view(
 ) -> Html {
     html! {
         <>
-    <svg version="1.1"
-     width={(W * LEN).to_string()} height={(H * LEN).to_string()}
-     xmlns="http://www.w3.org/2000/svg">
-        {field_view(field)}
-        {for chans.iter().filter_map(|chan|{
-            if let Ok(chan) = chan {
-                Some(chan_view(chan))
-            } else {
-                None
-            }
-        })}
+        <svg version="1.1"
+        width={(W * LEN).to_string()} height={(H * LEN).to_string()}
+        xmlns="http://www.w3.org/2000/svg">
+            {field_view(field)}
+            {for chans.iter().filter_map(|chan|{
+                if let Ok(chan) = chan {
+                    Some(chan_view(chan))
+                } else {
+                    None
+                }
+            })}
         </svg>
         </>
     }
@@ -427,10 +396,10 @@ fn circle(pos: (usize, usize), r: usize, fill: String) -> Html {
 fn chan_view(
     Chan {
         pos,
-        hp,
-        act,
-        mem,
-        ori,
+        hp: _,
+        gene: _,
+        mem: _,
+        ori: _,
     }: &Chan,
 ) -> Html {
     circle(
@@ -454,7 +423,7 @@ enum Msg {
 
 #[derive(Debug, Clone, PartialEq, Properties)]
 struct StartAppGame {
-    game: OneGame,
+    genes: Vec<Gene>,
 }
 
 impl Component for App {
@@ -463,7 +432,9 @@ impl Component for App {
     fn create(ctx: &Context<Self>) -> Self {
         let callback = ctx.link().callback(|_| Msg::Tick);
         let interval = Interval::new(10, move || callback.emit(()));
-        let StartAppGame { game } = ctx.props();
+        let StartAppGame { genes } = ctx.props();
+        let mut rng = thread_rng();
+        let game = OneGame::from_gene(genes.to_vec(), &mut rng);
         Self {
             game: game.clone(),
             interval,
@@ -482,7 +453,10 @@ impl Component for App {
                 true
             }
             Msg::End => {
-                self.game = self.game.end();
+                let mut rng = thread_rng();
+                let genes = self.game.get_genes_ordered();
+                let new_genes = traint_from_gene(genes, TRAIN_B, &mut rng);
+                self.game = OneGame::from_gene(new_genes, &mut rng);
                 true
             }
         }
